5779ce247dab46a4c417b6b658f069625715056a
soaurabh purohit <34468141+sourabh1857@users.noreply.github.com>
2021-04-14 10:30:07 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+  
+variable "s3_bucket_name" {
+   type = "list"
+   default = ["virtusa-s3-1", "virtusa-s3-2", "virtusa-s3-3"]
+}
+resource "aws_s3_bucket" "henrys_bucket" {
+   count = "${length(var.s3_bucket_name)}"
+   bucket = "${var.s3_bucket_name[count.index]}"
+   acl = "private"
+   versioning {
+      enabled = true
+   }
+   force_destroy = "true"
+}
